d1add2cf89407fb625438e6a434f5ac6bb4ff474
adambojanowski <131194168+adambojanowski@users.noreply.github.com>
2023-04-22 22:51:50 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace listy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4, 5 };
+            var names = new List<string>() { "Ala", "Jola" };
+            foreach (string na in names)
+            {
+                Console.WriteLine(na);
+                foreach (int nu in numbers)
+                {
+                    Console.WriteLine(nu);
+                }
+            }
+        }
+    }
+}
